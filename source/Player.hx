c48a97f9d8e674aac789b6e3cbfe11bc3a5407a2
Zura Khetsuriani <zura.khetsuriani@gmail.com>
2023-10-31 04:18:25 +0400

    Added the player sprite asset and set up animations. Removed the stub file.

--- source/Player.hx
+++ source/Player.hx
@@ -12,7 +12,20 @@ class Player extends FlxSprite
 	{
 		super(x, y);
 
-		makeGraphic(16, 16, FlxColor.BLUE);
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+		setFacingFlip(LEFT, false, false);
+		setFacingFlip(RIGHT, true, false);
+
+		// match the feet for the hitbox - bottom middle of the sprite
+		setSize(8, 8);
+		offset.set(4, 8);
+
+		animation.add("d_idle", [0]);
+		animation.add("lr_idle", [3]);
+		animation.add("u_idle", [6]);
+		animation.add("d_walk", [0, 1, 0, 2], 6);
+		animation.add("lr_walk", [3, 4, 3, 5], 6);
+		animation.add("u_walk", [6, 7, 6, 8], 6);
 
 		drag.x = drag.y = 800;
 	}
@@ -45,6 +58,7 @@ class Player extends FlxSprite
 			var newAngle:Float = 0; // -90 or 270 is up
 			if (up)
 			{
+				facing = UP;
 				newAngle = -90;
 				if (left)
 					newAngle -= 45;
@@ -53,6 +67,7 @@ class Player extends FlxSprite
 			}
 			else if (down)
 			{
+				facing = DOWN;
 				newAngle = 90;
 				if (left)
 					newAngle += 45;
@@ -60,11 +75,33 @@ class Player extends FlxSprite
 					newAngle -= 45;
 			}
 			else if (left)
+			{
+				facing = LEFT;
 				newAngle = 180;
+			}
 			else if (right)
+			{
+				facing = RIGHT;
 				newAngle = 0;
+			}
 
 			velocity.setPolarDegrees(SPEED, newAngle);
+		}
+
+		// "walk" if player is moving and not colliding with walls
+		var action = ((velocity.x != 0 || velocity.y != 0) && touching == NONE) ? "walk" : "idle";
+
+		// animation.play does not restart the matching animation if it is currently running
+
+		switch (facing)
+		{
+			case LEFT, RIGHT:
+				animation.play("lr_" + action);
+			case UP:
+				animation.play("u_" + action);
+			case DOWN:
+				animation.play("d_" + action);
+			default:
 		}
 	}
 }
