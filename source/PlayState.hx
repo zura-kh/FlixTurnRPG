1a65c83e84bddac8fcc1e97dd4c13bc5a69be265
Zura Khetsuriani <zura.khetsuriani@gmail.com>
2023-11-03 01:07:06 +0400

    Load the tilemap. Define tile properties for collisions. Set the player position in the tilemap. Apply collision detection for the player vs walls.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,21 +1,46 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState
 {
 	var player:Player;
+	var map:FlxOgmo3Loader;
+	var walls:FlxTilemap;
 
 	override public function create()
 	{
 		super.create();
 
-		player = new Player(20, 20);
+		// load room
+		map = new FlxOgmo3Loader(AssetPaths.flixTurnRPG__ogmo, AssetPaths.room_001__json);
+		walls = map.loadTilemap(AssetPaths.tiles__png, "walls");
+		walls.follow();
+		walls.setTileProperties(1, NONE); // tile 1: fool - no collide
+		walls.setTileProperties(2, ANY); // tile 2: walls - collide from any direction
+		add(walls);
+
+		player = new Player();
+		map.loadEntities(placeEntities, "entities");
+
 		add(player);
+	}
+
+	function placeEntities(entity:EntityData)
+	{
+		if (entity.name == "player")
+		{
+			player.setPosition(entity.x, entity.y);
+		}
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		FlxG.collide(player, walls);
 	}
 }
