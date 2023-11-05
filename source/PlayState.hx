7e5127230179c4217c794cebf9f9e415c6d7c16e
Zura Khetsuriani <zura.khetsuriani@gmail.com>
2023-11-05 14:46:43 +0400

    Added: coin entities in the tilemap, coin.png image, Coin class. Added the FlxTypedGroup of coins to PlayState. Player overlapping coins behavior: "kill" coins. Tween animation for coins "killing".

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState
@@ -10,6 +11,7 @@ class PlayState extends FlxState
 	var player:Player;
 	var map:FlxOgmo3Loader;
 	var walls:FlxTilemap;
+	var coins:FlxTypedGroup<Coin>;
 
 	override public function create()
 	{
@@ -22,6 +24,9 @@ class PlayState extends FlxState
 		walls.setTileProperties(1, NONE); // tile 1: fool - no collide
 		walls.setTileProperties(2, ANY); // tile 2: walls - collide from any direction
 		add(walls);
+
+		coins = new FlxTypedGroup<Coin>();
+		add(coins);
 
 		player = new Player();
 		map.loadEntities(placeEntities, "entities");
@@ -37,6 +42,10 @@ class PlayState extends FlxState
 		{
 			player.setPosition(entity.x, entity.y);
 		}
+		else if (entity.name == "coin")
+		{
+			coins.add(new Coin(entity.x + 4, entity.y + 4)); // centered on the tile
+		}
 	}
 
 	override public function update(elapsed:Float)
@@ -44,5 +53,15 @@ class PlayState extends FlxState
 		super.update(elapsed);
 
 		FlxG.collide(player, walls);
+
+		FlxG.overlap(player, coins, playerTouchCoin);
+	}
+
+	function playerTouchCoin(player:Player, coin:Coin)
+	{
+		if (player.alive && player.exists && coin.alive && coin.exists)
+		{
+			coin.kill();
+		}
 	}
 }
